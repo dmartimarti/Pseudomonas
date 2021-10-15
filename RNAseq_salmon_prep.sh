7e693c135f793262c45188fc347e2e3893e709b9
damarma <dmartimarti@gmail.com>
2021-10-15 10:38:22 +0100

    minor updates

--- RNAseq_salmon_prep.sh
+++ RNAseq_salmon_prep.sh
@@ -19,28 +19,14 @@ curl http://ftp.ensembl.org/pub/release-104/fasta/caenorhabditis_elegans/cdna/Ca
 # generate the index of the target transcriptome
 salmon index -t celegans.fa.gz -i celegans_index
 
-WORK=/rds/general/project/lms-cabreiro-raw/live/211013_VH00504_11_AAAMNH7M5/Unaligned
+### IMPORTANT
+# to process the data, use the scripts available in the repo
+# 1. trimm_seqs.pbs to quality filter
+# 2. salmon_hpc.pbs to process the filtered seqs into counts with Salmon
 
-# bioF mutant control
-salmon quant -i celegans_index -l A -1 $WORK/290921_JV_B0L1_S5_L001_R1_001.fastq.gz  -2 $WORK/290921_JV_B0L1_S5_L001_R2_001.fastq.gz  -p 8 --validateMappings --gcBias -o quants/B0_1
-salmon quant -i celegans_index -l A -1 $WORK/290921_JV_B0L2_S12_L001_R1_001.fastq.gz -2 $WORK/290921_JV_B0L2_S12_L001_R2_001.fastq.gz -p 8 --validateMappings --gcBias -o quants/B0_2
-salmon quant -i celegans_index -l A -1 $WORK/290921_JV_B0L3_S19_L001_R1_001.fastq.gz -2 $WORK/290921_JV_B0L3_S19_L001_R2_001.fastq.gz -p 8 --validateMappings --gcBias -o quants/B0_3
-salmon quant -i celegans_index -l A -1 $WORK/290921_JV_B0L4_S26_L001_R1_001.fastq.gz -2 $WORK/290921_JV_B0L4_S26_L001_R2_001.fastq.gz -p 8 --validateMappings --gcBias -o quants/B0_4
+## Copy the quants folder into my computer
+cd /mnt/d/MRC_Postdoc/Pseudomonas/RNAseq_pseudomonas
+# scp to my computer
+scp -r dmarti14@login.hpc.imperial.ac.uk:/rds/general/user/dmarti14/home/Pseudomonas/quants ./
 
-# bioF mutant Metformin
-salmon quant -i celegans_index -l A -1 $WORK/290921_JV_B50L1_S6_L001_R1_001.fastq.gz  -2 $WORK/290921_JV_B50L1_S6_L001_R2_001.fastq.gz  -p 8 --validateMappings --gcBias -o quants/B50_1
-salmon quant -i celegans_index -l A -1 $WORK/290921_JV_B50L2_S13_L001_R1_001.fastq.gz -2 $WORK/290921_JV_B50L2_S13_L001_R2_001.fastq.gz -p 8 --validateMappings --gcBias -o quants/B50_2
-salmon quant -i celegans_index -l A -1 $WORK/290921_JV_B50L3_S20_L001_R1_001.fastq.gz -2 $WORK/290921_JV_B50L3_S20_L001_R2_001.fastq.gz -p 8 --validateMappings --gcBias -o quants/B50_3
-salmon quant -i celegans_index -l A -1 $WORK/290921_JV_B50L4_S27_L001_R1_001.fastq.gz -2 $WORK/290921_JV_B50L4_S27_L001_R2_001.fastq.gz -p 8 --validateMappings --gcBias -o quants/B50_4
-
-# gacA mutant control
-salmon quant -i celegans_index -l A -1 $WORK/290921_JV_G0L1_S7_L001_R1_001.fastq.gz  -2 $WORK/290921_JV_G0L1_S7_L001_R2_001.fastq.gz  -p 8 --validateMappings --gcBias -o quants/G0_1
-salmon quant -i celegans_index -l A -1 $WORK/290921_JV_G0L2_S14_L001_R1_001.fastq.gz -2 $WORK/290921_JV_G0L2_S14_L001_R2_001.fastq.gz -p 8 --validateMappings --gcBias -o quants/G0_2
-salmon quant -i celegans_index -l A -1 $WORK/290921_JV_G0L3_S21_L001_R1_001.fastq.gz -2 $WORK/290921_JV_G0L3_S21_L001_R3_001.fastq.gz -p 8 --validateMappings --gcBias -o quants/G0_3
-salmon quant -i celegans_index -l A -1 $WORK/290921_JV_G0L4_S28_L001_R1_001.fastq.gz -2 $WORK/290921_JV_G0L4_S28_L001_R4_001.fastq.gz -p 8 --validateMappings --gcBias -o quants/G0_4
-
-
-
-
-
-
+### THE NEXT PARTS OF THE ANALYSIS ARE DONE IN R WITH DESEQ2
