a062cc922d98f26d6a3786b5ebc4c71c35ac76aa
damarma <dmartimarti@gmail.com>
2022-04-11 20:00:11 +0100

    Update app.R

--- app.R
+++ app.R
@@ -20,6 +20,8 @@ stats_shiny = read_csv('data/complete_stats.csv') %>%
     select(gene_id, gene_name, Contrast_description, Contrast, 
            Media:Reference, baseMean:Direction)
 
+stats_int = read_csv('data/interaction_stats.csv')
+
 
 theme_set(theme_cowplot(15))
 
@@ -30,6 +32,7 @@ ui <- fluidPage(
     title = "Normalised counts RNA seq",
     
         sidebarPanel(
+            # general
             selectizeInput('gene', choices = NULL, label = 'Gene',
                            multiple = TRUE, 
                            options = list(maxItems = 4, create = TRUE)),
@@ -38,6 +41,7 @@ ui <- fluidPage(
             sliderInput("width", label = "Plot width", 
                         min = 100, max = 1500, value = 500),
             br(),
+            # volcano plot
             h3('For volcano'),
             selectInput('contrast', label = 'Contrast', 
                         choices = unique(stats_shiny$Contrast)),
@@ -45,7 +49,14 @@ ui <- fluidPage(
             sliderInput("padj_thres", label = "Max -(log Padj)", 
                         min = 0.1, max = 80, value = 15),
             sliderInput("log2_thres", label = "Max log2FC", 
-                        min = 0.1, max = 50, value = 3.5)
+                        min = 0.1, max = 50, value = 3.5),
+            br(),
+            # interaction
+            h3('For interaction terms'),
+            h4('Select here the genes you want to plot'),
+            selectizeInput('gene_int', choices = NULL, label = 'Gene int',
+                           multiple = F, 
+                           options = list(create = TRUE))
     ),
     
         mainPanel(
@@ -67,12 +78,35 @@ ui <- fluidPage(
                     br(),
                     dataTableOutput("stats")
                 ),
+                
                 tabPanel("Volcano plot",
                     h3('Volcano plot'),
                     br(),
-                    plotlyOutput(outputId  = 'VolcanoPlot')), 
-                    br(),
-            )
+                    plotlyOutput(outputId  = 'VolcanoPlot')
+                    
+                ), 
+                    
+                
+                tabPanel('Interaction',
+                         h3('Interaction plots'),
+                         plotOutput('plot_int'),
+                         br(),
+                         br(),
+                         br(),
+                         br(),
+                         br(),
+                         br(),
+                         br(),
+                         br(),
+                         br(),
+                         br(),
+                         br(),
+                         dataTableOutput("stats_int")
+                )        
+            
+        ),
+            
+                
         )
             
 
@@ -81,13 +115,19 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
     
-    # update select size input
+    # update select size input for gene
     updateSelectizeInput(session, 'gene', label = 'Gene',
                          choices = unique(data_long$gene_name),
                          selected = 'argk-1',
                          server = TRUE)
     
-    output$plot <- renderPlot(
+    # update select size input for gene_int
+    updateSelectizeInput(session, 'gene_int', label = 'Gene int',
+                         choices = unique(data_long$gene_name),
+                         selected = 'argk-1',
+                         server = TRUE)
+    
+    output$plot = renderPlot(
         
         data_long %>%
             mutate(Replicate = factor(Replicate,
@@ -110,6 +150,7 @@ server <- function(input, output, session) {
         height = function() input$height
     ) 
     
+    #### Volcano plot ####
     output$VolcanoPlot = renderPlotly(
 
         {
@@ -147,6 +188,75 @@ server <- function(input, output, session) {
             
             }
         )
+    
+    
+    #### interaction plot ####
+    
+    # output$plot_int = renderPlot(
+    #     
+    #     gns = stats_int %>% filter(gene_id %in% input$gene_int),
+    #     
+    #     gns_stats = stats_int %>% 
+    #         filter(gene_id %in% gns) %>% 
+    #         mutate(p_adj_stars = ifelse(p_adj_stars==' ','no sig',p_adj_stars)),
+    #     
+    #     gns_counts = data_long %>%
+    #         dplyr::filter(gene_id %in% gns) %>%
+    #         filter(Sample %in% c('WT_0','WT_50','WTN_0','WTN_50')) %>% 
+    #         mutate(max_val = max(counts)),
+    #     
+    #     max_val = gns_counts$max_val[1],
+    #     
+    #     fc.wt = gns_stats %>% filter(contrast=='WT') %>% pull(log2FoldChange) %>% round(2),
+    #     fc.wtn = gns_stats %>% filter(contrast=='WTN') %>% pull(log2FoldChange)%>% round(2),
+    #     fc.int = gns_stats %>% filter(contrast=='interaction') %>% pull(log2FoldChange)%>% round(2),
+    #     
+    #     pval.wt = gns_stats %>% filter(contrast=='WT') %>% pull(p_adj_stars),
+    #     pval.wtn = gns_stats %>% filter(contrast=='WTN') %>% pull(p_adj_stars),
+    #     pval.int = gns_stats %>% filter(contrast=='interaction') %>% pull(p_adj_stars),
+    #     
+    #     
+    #     gns_counts %>% 
+    #         ggplot(aes(y = counts, x = Sample)) +
+    #         geom_boxplot(aes(fill = Sample),
+    #                      outlier.colour = NULL,
+    #                      outlier.shape = NA) +
+    #         geom_point(position = position_jitter(width = 0.2)) +
+    #         stat_summary(
+    #             geom = "point",
+    #             fun = "mean",
+    #             col = "black",
+    #             size = 6,
+    #             shape = 21,
+    #             fill = "blue"
+    #         ) +
+    #         # WT segment
+    #         geom_segment(aes(x = 1, y = max_val*1.05, 
+    #                          xend = 2, yend = max_val*1.05),
+    #                      colour = 'black', size = 1) +
+    #         annotate('text', x = 1.5, y = max_val*1.07,
+    #                  label = glue('log2FC:{fc.wt}, pval:{pval.wt}')) +
+    #         # WTN segment
+    #         geom_segment(aes(x = 3, y = max_val*1.05, 
+    #                          xend = 4, yend = max_val*1.05),
+    #                      colour = 'black', size = 1) +
+    #         annotate('text', x = 3.5, y = max_val*1.07,
+    #                  label = glue('log2FC:{fc.wtn}, pval:{pval.wtn}')) +
+    #         # Int segment
+    #         geom_segment(aes(x = 1.5, y = max_val*1.15, 
+    #                          xend = 3.5, yend = max_val*1.15),
+    #                      colour = 'red', size = 1) +
+    #         annotate('text', x = 2.5, y = max_val*1.17,
+    #                  label = glue('log2FC:{fc.int}, pval:{pval.int}')) +
+    #         facet_wrap(~gene_name, scales = 'free_y') +
+    #         labs(x = 'Sample',
+    #              y = 'Normalised counts (log scale)') +
+    #         theme_cowplot(15) +
+    #         panel_border() +
+    #         theme(axis.text.x = element_text(angle=45, hjust = 1))
+    # 
+    # )
+    
     
     
     output$stats = renderDataTable(stats_shiny %>% filter(gene_name %in% 
