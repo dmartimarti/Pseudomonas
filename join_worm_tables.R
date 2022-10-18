a52d1ecd0e6f582ea8fccd32c20733ed2399cf99
dmartimarti <dmartimarti@gmail.com>
2022-10-18 15:44:44 +0100

    Update join_worm_tables.R

--- join_worm_tables.R
+++ join_worm_tables.R
@@ -43,7 +43,58 @@ for (file in file_metadata$file) {
 file_metadata %>% 
   left_join(merge_file) %>% view
 
+merge_file = file_metadata %>% 
+  left_join(merge_file) 
+
+# fix the plate number for ExMr 15-3
+merge_file = merge_file %>% 
+  mutate(plate_number = case_when(plate_number == '15-3' ~ '15_3',
+                                  TRUE ~ plate_number),
+         plate_type = case_when(plate_type == 'PAMR' ~ 'PAMr',
+                                TRUE ~ plate_type))
+
 # save the table
-file_metadata %>% 
-  left_join(merge_file) %>% 
+merge_file %>% 
   write_csv('merge_tables.csv')
+
+
+
+
+# merge with gene info ----------------------------------------------------
+
+
+library(readxl)
+pa14 = read_excel("PA14_TP_annotation.xls")
+
+controls = read_excel("PATP_Control_plate.xlsx", 
+                      sheet = "control_list") %>% 
+  mutate(plate_number = as.character(plate_number))
+
+pa_metadata = pa14 %>% 
+  distinct(PA14_plate_nr) %>% 
+  mutate(Plate_type = str_split(PA14_plate_nr, n = 4, pattern = '_')) %>% 
+  unnest_wider(Plate_type)
+
+pa_metadata = pa_metadata %>% 
+  select(-`...3`, -`...2`) %>% 
+  rename(plate_type = `...1`, plate_number = `...4`)
+
+pa14 = pa14 %>% 
+  left_join(pa_metadata) %>% 
+  bind_rows(controls)
+
+
+pa14 %>% write_csv('PA14_gene_list_extended.csv')
+
+
+
+
+# join the tables ---------------------------------------------------------
+
+
+
+merge_file %>% 
+  left_join(pa14) %>% 
+  select(plate_type, well, plate_number, everything()) %>% 
+  write_csv('worms_metadata_181022.csv')
+
