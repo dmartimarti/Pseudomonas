764d621f21b64594caf89f57d01f8d1375d7e486
dmartimarti <dmartimarti@gmail.com>
2022-10-18 12:53:39 +0100

    Create join_worm_tables.R

---
+++ join_worm_tables.R
@@ -0,0 +1,49 @@
+library(tidyverse)
+library(readr)
+library(openxlsx)
+library(glue)
+
+# read the list of files and create a tibble
+files = list.files('inputs/', pattern = '.csv') %>% as_tibble() %>% 
+  rename(file = value)
+
+# specify new columns
+new_col_names = c(
+  'file',
+  'replicate',
+  'date',
+  'plate_type',
+  'plate_number',
+  'well',
+  'zstack',
+  'channel'
+  )
+
+# split the filenames and unnest them
+file_metadata = files %>% 
+  mutate(col_info = str_split(file, pattern = '_')) %>% 
+  unnest_wider(col_info) %>% 
+  select(-`...8`)
+# change column names for the new ones
+colnames(file_metadata)  = new_col_names
+
+
+# loop to read each file and add the file name, it will
+# help to join the metadata
+merge_file = tibble()
+for (file in file_metadata$file) {
+  temp = read_csv(glue('inputs/{file}')) %>% 
+    mutate(file = file,
+           `User Label` = as.character(`User Label`))
+  
+  merge_file = bind_rows(merge_file, temp)
+}
+
+# chech everything is ok
+file_metadata %>% 
+  left_join(merge_file) %>% view
+
+# save the table
+file_metadata %>% 
+  left_join(merge_file) %>% 
+  write_csv('merge_tables.csv')
